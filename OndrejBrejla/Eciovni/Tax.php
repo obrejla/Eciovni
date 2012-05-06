9f1630f42c414371887b438c970224199a452c1c
Ondrej Brejla <ondrej@brejla.cz>
2012-05-06 11:53:27 +0200

    PHPDoc added.

--- OndrejBrejla/Eciovni/Tax.php
+++ OndrejBrejla/Eciovni/Tax.php
@@ -11,6 +11,12 @@ namespace OndrejBrejla\Eciovni;
  */
 interface Tax {
 
+    /**
+     * Returns tax in a upper decimal format.
+     * I.e. '1.22' for '22%'.
+     *
+     * @return double
+     */
     public function inUpperDecimal();
 
 }
--- OndrejBrejla/Eciovni/TaxImpl.php
+++ OndrejBrejla/Eciovni/TaxImpl.php
@@ -14,12 +14,25 @@ use InvalidArgumentException;
  */
 class TaxImpl extends Object implements Tax {
 
+    /** @var double */
     private $taxInUpperDecimal;
 
+    /**
+     * Creates new TaxImpl object.
+     *
+     * @param double $upperDecimal
+     */
     private function __construct($upperDecimal) {
         $this->taxInUpperDecimal = $upperDecimal;
     }
 
+    /**
+     * Creates new Tax from a percent value.
+     *
+     * @param int|double $percent
+     * @return Tax
+     * @throws InvalidArgumentException
+     */
     public static function fromPercent($percent) {
         if ($percent === NULL) {
             throw new InvalidArgumentException('$percent can not be null.');
@@ -27,6 +40,14 @@ class TaxImpl extends Object implements Tax {
         return new TaxImpl(($percent * 0.01) + 1);
     }
 
+    /**
+     * Creates new Tax from a lower decimal value.
+     * I.e. value must be '0.22' for a tax of '22%'.
+     *
+     * @param double $lowerDecimal
+     * @return Tax
+     * @throws InvalidArgumentException
+     */
     public static function fromLowerDecimal($lowerDecimal) {
         if (!is_double($lowerDecimal)) {
             throw new InvalidArgumentException('$lowerDecimal must be a double, but ' . $lowerDecimal . ' given.');
@@ -34,6 +55,14 @@ class TaxImpl extends Object implements Tax {
         return new TaxImpl($lowerDecimal + 1);
     }
 
+    /**
+     * Creates new Tax from a upper decimal value.
+     * I.e. value must be '1.22' for a tax of '22%'.
+     *
+     * @param double $lowerDecimal
+     * @return Tax
+     * @throws InvalidArgumentException
+     */
     public static function fromUpperDecimal($upperDecimal) {
         if (!is_double($upperDecimal)) {
             throw new InvalidArgumentException('$upperDecimal must be a double, but ' . $upperDecimal . ' given.');
@@ -41,6 +70,12 @@ class TaxImpl extends Object implements Tax {
         return new TaxImpl($upperDecimal);
     }
 
+    /**
+     * Returns tax in a upper decimal format.
+     * I.e. '1.22' for '22%'.
+     *
+     * @return double
+     */
     public function inUpperDecimal() {
         return $this->taxInUpperDecimal;
     }
