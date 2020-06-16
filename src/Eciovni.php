0807aa9626ce075db43d4462e06434e648678d73
Jan Barášek <janbarasek@gmail.com>
2020-06-16 14:15:02 +0200

    Latte: Added number formating for thousands and &nbsp;.

--- src/Eciovni.php
+++ src/Eciovni.php
@@ -213,7 +213,7 @@ class Eciovni
 	{
 		return (new Engine)
 			->addFilter('round', static function (float $value, int $precision = 2) {
-				return number_format(round($value, $precision), $precision, ',', '');
+				return number_format(round($value, $precision), $precision, ',', ' ');
 			});
 	}
 }
