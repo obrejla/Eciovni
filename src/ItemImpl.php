13fbe5bc93cf80cd3af6115bf085dfeba27a9242
Jan Barášek <janbarasek@gmail.com>
2021-01-12 21:45:11 +0100

    ItemImpl: Rename units to count + unitValue to unitPrice.

--- src/ItemImpl.php
+++ src/ItemImpl.php
@@ -9,22 +9,22 @@ class ItemImpl
 {
 	private string $description;
 
-	private int $units;
+	private int $count;
 
-	private float $unitValue;
+	private float $unitPrice;
 
 	private Tax $tax;
 
 	private bool $unitValueIsTaxed;
 
 
-	public function __construct(string $description, int $units, float $unitValue, Tax $tax, bool $unitValueIsTaxed = true)
+	public function __construct(string $description, int $count, float $unitPrice, Tax $tax, bool $unitPriceIsTaxed = true)
 	{
 		$this->description = $description;
-		$this->units = $units;
-		$this->unitValue = $unitValue;
+		$this->count = $count;
+		$this->unitPrice = $unitPrice;
 		$this->tax = $tax;
-		$this->unitValueIsTaxed = $unitValueIsTaxed;
+		$this->unitValueIsTaxed = $unitPriceIsTaxed;
 	}
 
 
@@ -40,16 +40,23 @@ class ItemImpl
 	}
 
 
+	public function getUnitPrice(): float
+	{
+		return $this->unitPrice;
+	}
+
+
+	/**
+	 * @deprecated since 2021-01-12 use getUnitPrice().
+	 */
 	public function getUnitValue(): float
 	{
-		return $this->unitValue;
+		return $this->getUnitPrice();
 	}
 
 
 	/**
 	 * Returns TRUE, if the unit value is taxed (otherwise FALSE).
-	 *
-	 * @return bool
 	 */
 	public function isUnitValueTaxed(): bool
 	{
@@ -57,36 +64,45 @@ class ItemImpl
 	}
 
 
+	public function getCount(): int
+	{
+		return $this->count;
+	}
+
+
+	/**
+	 * @deprecated since 2021-01-12 use getCount().
+	 */
 	public function getUnits(): int
 	{
-		return $this->units;
+		return $this->getCount();
 	}
 
 
 	public function countTaxValue(): float
 	{
-		return ($this->countTaxedUnitValue() - $this->countUntaxedUnitValue()) * $this->getUnits();
+		return ($this->countTaxedUnitValue() - $this->countUntaxedUnitValue()) * $this->getCount();
 	}
 
 
 	public function countUntaxedUnitValue(): float
 	{
 		return $this->isUnitValueTaxed()
-			? $this->getUnitValue() / $this->getTax()->inUpperDecimal()
-			: $this->getUnitValue();
+			? $this->getUnitPrice() / $this->getTax()->inUpperDecimal()
+			: $this->getUnitPrice();
 	}
 
 
 	public function countFinalValue(): float
 	{
-		return $this->getUnits() * $this->countTaxedUnitValue();
+		return $this->getCount() * $this->countTaxedUnitValue();
 	}
 
 
 	private function countTaxedUnitValue(): float
 	{
 		return $this->isUnitValueTaxed()
-			? $this->getUnitValue()
-			: $this->getUnitValue() * $this->getTax()->inUpperDecimal();
+			? $this->getUnitPrice()
+			: $this->getUnitPrice() * $this->getTax()->inUpperDecimal();
 	}
 }
